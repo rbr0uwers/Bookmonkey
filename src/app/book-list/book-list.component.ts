10827ac105108be54e5b546a9a06040538b83181
Ronny Brouwers <ronny.brouwers@icloud.com>
2021-05-17 18:16:22 +0200

    Introduced Book-Store Service

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,45 +1,21 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Book } from '../shared/book';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'bm-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css']
 })
+
 export class BookListComponent implements OnInit {
   books!: Book[];
   @Output() showDetailsEvent = new EventEmitter<Book>();
-  constructor() { }
+  
+  constructor(private bs : BookStoreService) { }
 
   ngOnInit(): void {
-    this.books = [
-      {
-        isbn: '9783864907791',
-        title: 'Angular',
-        authors: ['Ferdinand Malcher', 'Johannes Hoppe', 'Danny Kopenhagen'],
-        published: new Date(2020, 8, 1),
-        subtitle: 'Grundlagen, fortgeschrittene Themen und Best Practices',
-        rating: 5,
-        thumbnails: [{
-          url: 'https://ng-buch.de/angular-cover.jpg',
-          title: 'Buchcover'
-        }],
-        description: 'Lernen Sie Angular mit diesem Praxisbuch!'
-      },
-      {
-        isbn: '9783864905520',
-        title: 'React',
-        authors: ['Oliver Zweigermann', 'Nils Hartmann'],
-        published: new Date(2019, 11, 12),
-        subtitle: 'Grundlagen, fortgeschrittene Techniken und Praxistipps',
-        rating: 3,
-        thumbnails: [{
-          url: 'https://ng-buch.de/react-cover.jpg',
-          title: 'Buchcover'
-        }],
-        description: 'Das bewaehrte und umfassende Praxisbuch zu React.'
-      }
-    ]
+    this.books = this.bs.getAll();
   }
 
   showDetails(book: Book) {
---
+++ src/app/shared/book-store.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { Book } from './book';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookStoreService {
+  books : Book[];
+
+  constructor() { 
+    this.books = [
+      {
+        isbn: '9783864907791',
+        title: 'Angular',
+        authors: ['Ferdinand Malcher', 'Johannes Hoppe', 'Danny Kopenhagen'],
+        published: new Date(2020, 8, 1),
+        subtitle: 'Grundlagen, fortgeschrittene Themen und Best Practices',
+        rating: 5,
+        thumbnails: [{
+          url: 'https://ng-buch.de/angular-cover.jpg',
+          title: 'Buchcover'
+        }],
+        description: 'Lernen Sie Angular mit diesem Praxisbuch!'
+      },
+      {
+        isbn: '9783864905520',
+        title: 'React',
+        authors: ['Oliver Zweigermann', 'Nils Hartmann'],
+        published: new Date(2019, 11, 12),
+        subtitle: 'Grundlagen, fortgeschrittene Techniken und Praxistipps',
+        rating: 3,
+        thumbnails: [{
+          url: 'https://ng-buch.de/react-cover.jpg',
+          title: 'Buchcover'
+        }],
+        description: 'Das bewaehrte und umfassende Praxisbuch zu React.'
+      }
+    ]
+  }
+
+  getAll() : Book[] {
+    return this.books;
+  }
+}
