89989d75170a592db7c3c1a7a30cd14d7dc9f7e9
Ronny Brouwers <ronny.brouwers@icloud.com>
2021-06-19 17:06:45 +0200

    Book-Raw type introduced

--- src/app/shared/book-store.service.ts
+++ src/app/shared/book-store.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { Book } from './book';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { BookRaw } from './book-raw';
+import { BookFactory } from './book-factory';
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +15,19 @@ export class BookStoreService {
   constructor( private http: HttpClient) { }
 
   getAll() : Observable<Book[]> {
-    return this.http.get<any[]>(`${this.api}/books`);
+    return this.http.get<BookRaw[]>(`${this.api}/books`)
+      .pipe(
+        map(booksRaw => 
+          booksRaw.map(b => BookFactory.fromRaw(b))
+        )
+      );
   }
 
   getSingle(isbn: string) : Observable<Book> {
-    return this.http.get<any>(`${this.api}/book/${isbn}`);
+    return this.http.get<BookRaw>(`${this.api}/book/${isbn}`)
+      .pipe(
+        map(b => BookFactory.fromRaw(b))
+      );
   }
 
   remove(isbn: string) : Observable<any> {
